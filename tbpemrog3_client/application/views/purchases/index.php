a6b0bb6e5ee7be6b6934f29b6f8b84a21f7bf286
DenjanD <alfien311@gmail.com>
2022-07-26 10:59:45 +0700

    Atur template client, base url, dan index Purchases

--- tbpemrog3_client/application/views/purchases/index.php
+++ tbpemrog3_client/application/views/purchases/index.php
@@ -1,27 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>List Data Pembelian</title>
-</head>
-<body>
-    <table>
-        <tr>
-            <th>Tanggal</th>
-            <th>Total</th>
-            <th>Deskripsi</th>
-            <th>Supplier</th>
-        </tr>
-    </table>
-    <?php foreach($data_pembelian as $dp): ?>
-        <tr>
-            <td><?= $dp['date'] ?></td>
-            <td><?= $dp['total'] ?></td>
-            <td><?= $dp['description'] ?></td>
-            <td><?= $dp['name'] ?></td>
-        </tr>
-    <?php endforeach; ?>
+<html>
+    <?php $this->load->view('template/head'); ?>
+<body class="sb-nav-fixed">
+    <?php $this->load->view('template/navbarsidebar'); ?>
+    <div id="layoutSidenav_content">
+        <table class="table">
+            <tr>
+                <th>Tanggal</th>
+                <th>Total</th>
+                <th>Deskripsi</th>
+                <th>Supplier</th>
+            </tr>
+        <?php foreach($data_pembelian as $dp): ?>
+            <tr>
+                <td><?= $dp['date'] ?></td>
+                <td><?= $dp['total'] ?></td>
+                <td><?= $dp['description'] ?></td>
+                <td><?= $dp['name'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+        </table>
+    </div>
+    <?php $this->load->view('template/footer'); ?>
 </body>
 </html>
