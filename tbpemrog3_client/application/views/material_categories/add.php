3e83153e0e4a13856f337a5236764839dfb45de7
Zian Asti Dwiyanti <ziandwiasti23@gmail.com>
2022-07-27 21:48:32 +0700

    tambah function detail, edit, delete, add Material Categories

--- tbpemrog3_client/application/views/material_categories/add.php
+++ tbpemrog3_client/application/views/material_categories/add.php
@@ -1,50 +1,33 @@
-<div class="container pt-5">
-    <h3><?= $title ?></h3>
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb ">
-            <li class="breadcrumb-item"><a>Material Category</a></li>
-            <li class="breadcrumb-item "><a href="<?= base_url('material_categories'); ?>">List Data</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Add Data</li>
-        </ol>
-    </nav>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body">
-                    <?php
-                    //create form
-                    $attributes = array('method' => "post", "autocomplete" => "off");
-                    echo form_open('', $attributes);
-                    ?>
-                    <div class="form-group row">
-                        <label for="category_id" class="col-sm-2 col-form-label">Category ID</label>
-                        <div class="col-sm-5">
-                            <input type="text" class="form-control" id="category_id" name="category_id" value="<?= set_value('category_id'); ?>">
-                            <small class="text-danger">
-                                <?php echo form_error('category_id') ?>
-                            </small>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="name" class="col-sm-2 col-form-label">Name</label>
-                        <div class="col-sm-10">
-                            <input type="text" class="form-control" id="name" name="name" value=" <?= set_value('name'); ?>">
-                            <small class="text-danger">
-                                <?php echo form_error('name') ?>
-                            </small>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <div class="col-sm-10 offset-md-2">
-                            <button type="submit" class="btn btn-primary">Simpan</button>
-                            <a class="btn btn-secondary" href="javascript:history.back()">Kembali</a>
-                        </div>
-                    </div>
-                    </form>
-                </div>
+<html>
+    <?php $this->load->view('template/head'); ?>
+<body class="sb-nav-fixed">
+    <?php $this->load->view('template/navbarsidebar'); ?>
+    <div id="layoutSidenav_content">
+        <!-- Ngubah isi konten halaman dari sini -->
+        <div class="container">
+            <div class="row mt-2">
+                <h3>Tambah Data Kategori material</h3>
             </div>
-        </div>
+            <hr>
+            <div class="row mt-2">
+                <form action="<?= base_url('material_categories/add') ?>" method="post">
+                <div class="col-md-6">                    
+                    <label for="">ID Kategori</label>
+                    <input type="number" class="form-control" name="category_id">                   
+                </div>
+                <div class="col-md-6 mt-2">                    
+                    <label for="">Nama Kategori</label>
+                    <input type="text" class="form-control" name="name">                   
+                </div>
+                <div class="col-md-6">
+                    <a href="<?= base_url('material_categories') ?>" class="btn btn-secondary mt-2">Kembali</a>
+                    <input type="submit" class="btn btn-primary float-end mt-2" value="Simpan">
+                </div>
+                </form>
+            </div>
+        </div>       
+        <!-- Ngubah isi konten halaman sampe sini -->
     </div>
-</div>
+    <?php $this->load->view('template/footer'); ?>
+</body>
+</html>
--- tbpemrog3_client/application/views/material_categories/index.php
+++ tbpemrog3_client/application/views/material_categories/index.php
@@ -1,60 +1,76 @@
-<div class="container pt-5">
-    <h3><?= $title ?></h3>
-    <div class="flash-data" data-flashdata="<?= $this->session->flashdata('flash'); ?>"></div>
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb ">
-            <li class="breadcrumb-item"><a>Material Categories</a></li>
-            <li class="breadcrumb-item active" aria-current="page">List Data</li>
-        </ol>
-    </nav>
-    <div class="row">
-        <div class="col-md-12">
-            <a class="btn btn-primary mb-2" href="<?= base_url('material_categories/add')?>">Tambah Data</a>
-            <div mb-2>
-                <!-- Menampilkan flash data (pesan saat data error)-->
-                <?php if ($this->session->flashdata('message')) : ?>
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        Error! <?= $this->session->flashdata('message'); ?>
-                        <button type="button" class="close" data-dismiss="alert" arialabel="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                <?php endif; ?>
+<html>
+    <?php $this->load->view('template/head'); ?>
+<body class="sb-nav-fixed">
+    <?php $this->load->view('template/navbarsidebar'); ?>
+    <div id="layoutSidenav_content">
+        <!-- Ngubah isi konten halaman dari sini -->
+        <div class="container">
+            <div class="row mt-2">
+                <h3>Data Material Categories</h3>
             </div>
-            <div class="card">
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table table-striped table-bordered tablehover text-sm" id="tablecategories">
-                            <thead>
-                                <tr class="table-primary">
-                                    <th>CATEGORY ID</th>
-                                    <th>NAME</th>
-                                    <th>ACTION</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                foreach ($data_material_categories as $row) :
-                                ?>
-                                    <tr>
-                                        <td><?= $row['category_id'] ?></td>
-                                        <td><?= $row['name'] ?></td>
-                                        <td>
-                                            <a href="<?= base_url('material_categories/detail/'.$row['category_id'])?>" class="btn btn-primary btn-sm"><i class="fa fa-info"></i></a>
-                                            <a href="<?= base_url('material_categories/update/'.$row['category_id'])?>" class="btn btn-success btn-sm"><i class="fa fa-edit"></i></a>
-                                            <a href="<?= base_url('material_categories/delete/'.$row['category_id'])?>" class="btn btn-danger btn-sm item-delete tombol-hapus"><i class="fa fa-trash"></i></a>
-                                        </td>
-                                    </tr>
-                                <?php endforeach; ?>
-                            </tbody>
-                        </table>
-                    </div>
+            <hr>
+            <div class="row">
+                <div class="col-md-12">
+
+                    <!-- Alert kalau ada pesan dari controller -->
+                    <?php if ($this->session->flashdata('message')) : ?>
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            Error! <?= $this->session->flashdata('message'); ?>
+                            <button type="button" class="close" data-dismiss="alert" arialabel="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    <?php endif; ?>
+                    <!-- /Alert kalau ada pesan dari controller -->
+
+                    <a href="<?= base_url('material_categories/add') ?>" class="btn btn-success">Tambah Data</a>
+                    <table class="table">
+                        <tr>
+                            <th>ID Kategori</th>
+                            <th>Nama Kategori</th>
+                            <th>Aksi</th>
+                        </tr>
+                    <?php foreach($data_material_categories as $mc): ?>
+                        <tr>
+                            <td><?= $mc['category_id'] ?></td>
+                            <td><?= $mc['name'] ?></td>
+                            <td>
+                            <a href="<?= base_url('material_categories/detail/'.$mc['category_id'])?>" class="btn btn-secondary btn-sm"><i class="fa fa-info"></i></a>
+                                
+                            <a href="<?= base_url('material_categories/update/'.$mc['category_id'])?>" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i></a>
+                                <form action="<?= base_url('material_categories/delete/').$mc['category_id'] ?>" id="formDelete<?= $mc['category_id'] ?>">
+                                    <a href="#" onclick="deleteConfirmation(<?= $mc['category_id'] ?>)" class="btn btn-danger btn-sm">
+                                        <i class="fa fa-trash"></i>
+                                    </a>
+                                </form>    
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                    </table>
                 </div>
             </div>
-        </div>
+        </div>      
+        <!-- Ngubah isi konten halaman sampe sini -->
     </div>
-</div>
-<script>
-    //menampilkan data ketabel dengan plugin datatables
-    $('#tablecategories').DataTable();
-</script>
+    <?php $this->load->view('template/footer'); ?>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        function deleteConfirmation(id) {
+            var idData = id
+            Swal.fire({
+                title: 'Hapus data?',
+                text: "Anda yakin ingin menghapus data ini?",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Hapus Data'
+                }).then((result) => {
+                if (result.isConfirmed) {
+                    document.getElementById('formDelete'+idData).submit()
+                }
+            })
+        }
+    </script>
+</body>
+</html>
