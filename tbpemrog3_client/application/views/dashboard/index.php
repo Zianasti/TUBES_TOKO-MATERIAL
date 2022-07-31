1d475dc74f6902b40d82bd39b35aa1f5ccac9ff3
DenjanD <alfien311@gmail.com>
2022-07-31 16:49:20 +0700

    Penambahan dan penyesuaian menu template

---
+++ tbpemrog3_client/application/views/dashboard/index.php
@@ -0,0 +1,19 @@
+<html>
+    <?php $this->load->view('template/head'); ?>
+<body class="sb-nav-fixed">
+    <?php $this->load->view('template/navbarsidebar'); ?>
+    <div id="layoutSidenav_content">
+        <!-- Ngubah isi konten halaman dari sini -->
+        <div class="container">
+            <div class="row mt-2">
+                <h3>Dashboard</h3>
+            </div>
+            <hr>
+
+        </div>      
+        <!-- Ngubah isi konten halaman sampe sini -->
+    </div>
+    <?php $this->load->view('template/footer'); ?>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</body>
+</html>
